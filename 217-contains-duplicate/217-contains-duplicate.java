ca4ca2ee05fbafeba310a438e66ecc0961c89501
aathi2002 <68262020+aathi2002@users.noreply.github.com>
2022-07-08 14:21:41 +0530

    Time: 32 ms (7.96%), Space: 69.3 MB (52.91%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -9,15 +9,30 @@ class Solution {
 //             }
 //         }
         
-//         return false;
-        LinkedHashSet<Integer>aathi=new LinkedHashSet<>();
+// //         return false;
+//         LinkedHashSet<Integer>aathi=new LinkedHashSet<>();
+//         for(int i=0;i<nums.length;i++){
+//             aathi.add(nums[i]);
+            
+//         }
+//         if(aathi.size()==nums.length){
+//             return false;
+//         }
+//         return true;
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
-            aathi.add(nums[i]);
+                
+            if(i==nums.length-1){
+                break;
+            }
+            
+                
+            
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
             
         }
-        if(aathi.size()==nums.length){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
